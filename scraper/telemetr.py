450ffc993fb2b9a66c92688942b64186ad9f18b7
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-11-03 14:14:45 +0100

    upgrade get_channels_by_category_page

--- scraper/telemetr.py
+++ scraper/telemetr.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 from bs4 import BeautifulSoup
 import datetime as dt
 
-from RedisCache.cache import cache_decorator
+from RedisCache.cache import redis_cache
 
 
 @dataclass
@@ -37,108 +37,123 @@ class ScrapChannel:
         return self.__str__()
 
 
+@dataclass
+class ScrapCategories:
+
+    title: str
+    link_telemetr: str
+
+    def __str__(self):
+        return f'<{self.__class__.__name__}>: {self.title}'
+
+    def __repr__(self):
+        return self.__str__()
+
+
 def clear_text(text):
     return re.sub(r'[\n\t\']', '', text).strip()
 
 
-@cache_decorator(ignore_keys=['session'])
+@redis_cache(ignore_keys=['session'])
 def get_channels_by_category_page(
         *,
         session: requests.Session,
         category_url: str,
         page: int,
-        max_participants=1_000_000) -> List[ScrapChannel]:
+        max_participants: int = 1_000_000,
+        lang_code: str = 'any') -> List[ScrapChannel]:
 
     channels = []
     params = dict(
         page=page,
         participants_to=max_participants,
-        lang_code='any',
+        lang_code=lang_code,
     )
 
     response = session.get(category_url, params=params)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     channels_table = soup.find('table', id='channels_table')
-    channels_tbody = channels_table.find('tbody')
-    channels_tbody_tr = channels_tbody.find_all('tr')
+    if channels_table:
+        channels_tbody = channels_table.find('tbody')
+        channels_tbody_tr = channels_tbody.find_all('tr')
 
-    channel_trs = [[channels_tbody_tr[num], channels_tbody_tr[num+1]] for num, tr in enumerate(channels_tbody_tr) if not num % 2]
+        channel_trs = [[channels_tbody_tr[num], channels_tbody_tr[num+1]] for num, tr in enumerate(channels_tbody_tr) if not num % 2]
 
-    for data in channel_trs:
-        tr_main_data, tr_categories = data
+        for data in channel_trs:
+            tr_main_data, tr_categories = data
 
-        photo_td = tr_main_data.find('td', 'text-center wd-100 pb-0')
+            photo_td = tr_main_data.find('td', 'text-center wd-100 pb-0')
 
-        photo_block = photo_td.select_one('img') if photo_td else None
-        photo = photo_block.get('src') if photo_block else None
+            photo_block = photo_td.select_one('img') if photo_td else None
+            photo = photo_block.get('src') if photo_block else None
 
-        channel_data = tr_main_data.select_one('td.wd-300.pb-0')
-        channel_a = channel_data.select_one('a')
-        link_telemetr = f'https://telemetr.me{channel_a.get("href", "")}'
-        title = channel_a.get_text(strip=True)
+            channel_data = tr_main_data.select_one('td.wd-300.pb-0')
+            channel_a = channel_data.select_one('a')
+            link_telemetr = f'https://telemetr.me{channel_a.get("href", "")}'
+            title = channel_a.get_text(strip=True)
 
-        data_cid = channel_data.select_one('button').get('data-cid')
-        channel_id = int(data_cid) if data_cid else None
+            data_cid = channel_data.select_one('button').get('data-cid')
+            channel_id = int(data_cid) if data_cid else None
 
-        link_tg = channel_data.select_one('a.btn.btn-sm.btn-xs.a_icon_black').get('data-link')
-        description_block = channel_data.select_one('div.text-nowrap.pt-3')
+            link_tg = channel_data.select_one('a.btn.btn-sm.btn-xs.a_icon_black').get('data-link')
+            description_block = channel_data.select_one('div.text-nowrap.pt-3')
 
-        try:
-            description = (description_block
-                           .select_one('span.btn.btn-outline-warning.btn-sm.btn-xs.kt-font-dark')
-                           .get('data-cont')
-                           .replace('<br>', '\n')) if description_block else ''
-        except AttributeError:
-            description = None
+            try:
+                description = (description_block
+                               .select_one('span.btn.btn-outline-warning.btn-sm.btn-xs.kt-font-dark')
+                               .get('data-cont')
+                               .replace('<br>', '\n')) if description_block else ''
+            except AttributeError:
+                description = None
 
-        td_all = tr_main_data.find_all('td', class_='text-nowrap pb-0')
+            td_all = tr_main_data.find_all('td', class_='text-nowrap pb-0')
 
-        participants_str = td_all[0].get_text(strip=True)
-        participants = int(participants_str.replace('\'','')) if participants_str else 1
+            participants_str = td_all[0].get_text(strip=True)
+            participants = int(participants_str.replace('\'','')) if participants_str else 1
 
-        views_block = td_all[2]
+            views_block = td_all[2]
 
-        views_data = []
-        views_spans = views_block.find_all('span')
-        for span in views_spans:
-            views_data.append(int(span.text.replace('\'', '')) if span else 0)
-        if not views_data or len(views_data) != 2:
-            views_data = [0, 0]
+            views_data = []
+            views_spans = views_block.find_all('span')
+            for span in views_spans:
+                views_data.append(int(span.text.replace('\'', '')) if span else 0)
+            if not views_data or len(views_data) != 2:
+                views_data = [0, 0]
 
-        views, views24 = views_data
+            views, views24 = views_data
 
-        categories_a = (tr_categories.find('td').find('div', class_='web-hide pb-2')
-                        .find_all('a', class_='btn btn-label-facebook btn-sm pl-2 pr-2 pt-1 pb-1 kt-font-12'))
-        categories = [c.text.split(' ')[0] for c in categories_a]
+            categories_a = (tr_categories.find('td').find('div', class_='web-hide pb-2')
+                            .find_all('a', class_='btn btn-label-facebook btn-sm pl-2 pr-2 pt-1 pb-1 kt-font-12'))
+            categories = [c.text.split(' ')[0] for c in categories_a]
 
-        try:
-            er = round((views / participants) * 100, 1)
-            er24 = round((views24 / participants) * 100, 1)
-        except ZeroDivisionError:
-            er = 0
-            er24 = 0
+            try:
+                er = round((views / participants) * 100, 1)
+                er24 = round((views24 / participants) * 100, 1)
+            except ZeroDivisionError:
+                er = 0
+                er24 = 0
 
-        channel = ScrapChannel(
-            id=channel_id,
-            title=title,
-            link_avatar=photo,
-            link_tg=link_tg,
-            link_telemetr=link_telemetr,
-            description=description,
-            participants=participants,
-            views=views,
-            views24=views24,
-            er=er,
-            er24=er24,
-            categories=categories,
-        )
-        channels.append(channel)
+            channel = ScrapChannel(
+                id=channel_id,
+                title=title,
+                link_avatar=photo,
+                link_tg=link_tg,
+                link_telemetr=link_telemetr,
+                description=description,
+                participants=participants,
+                views=views,
+                views24=views24,
+                er=er,
+                er24=er24,
+                categories=categories,
+            )
+            channels.append(channel)
 
     return channels
 
 
-@cache_decorator(ignore_keys=['session'])
+@redis_cache(ignore_keys=['session'])
 def get_channel_by_page(
         *,
         session: requests.Session,
