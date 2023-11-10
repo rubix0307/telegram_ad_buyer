a8e8e7f00489265ab5425bc1a8d82fa85a1bd47c
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-11-10 12:25:51 +0100

    rename first choice

--- channel/forms.py
+++ channel/forms.py
@@ -8,4 +8,4 @@ class ChannelForm(forms.Form):
     views__lte = forms.IntegerField(label='Просмотров До', min_value=0, required=False, widget=forms.NumberInput(attrs={'placeholder': '20 000'}))
     er__gte = forms.IntegerField(label='Вовлеченность От', min_value=0, required=False, widget=forms.NumberInput(attrs={'placeholder': '70'}))
     er__lte = forms.IntegerField(label='Вовлеченность До', min_value=0, required=False, widget=forms.NumberInput(attrs={'placeholder': '100%'}))
-    lang_code = forms.ChoiceField(label='Язык канала', choices=[('', 'Выберите...'), ('ru', 'Русский'), ('ua', 'Украинский')], required=False)
+    lang_code = forms.ChoiceField(label='Язык канала', choices=[('', 'Любой'), ('ru', 'Русский'), ('ua', 'Украинский')], required=False)
