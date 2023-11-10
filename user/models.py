4167c61e8d25be3ed8afe1787da129f9e73420da
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-11-10 18:33:08 +0100

    add db name

--- user/models.py
+++ user/models.py
@@ -39,20 +39,25 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
         related_name="custom_user_set",
         related_query_name="user",
+        db_table='user_groups',
     )
-    user_permissions = models.ManyToManyField(
+    permissions = models.ManyToManyField(
         'auth.Permission',
         verbose_name='user_permissions',
         blank=True,
         help_text='Specific permissions for this user.',
         related_name='custom_user_set',
         related_query_name='user',
+        db_table='user_permissions',
     )
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        db_table = 'user'
 
     def __str__(self):
         return self.email
