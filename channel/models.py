c878322359c697546379b29f4624d0f3ba68719f
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-11-11 06:51:40 +0100

    add is_show in category

--- channel/models.py
+++ channel/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255, unique=True)
     link_telemetr = models.URLField()
+    is_show = models.BooleanField(default=False)
 
     def __str__(self):
         return f'<{self.__class__.__name__}>: {self.title}'
