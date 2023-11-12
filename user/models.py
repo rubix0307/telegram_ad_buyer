b95c7331b1d1d8bbe3b0b071e4470f68a8af5c87
rubix0307 <miroshnichenkoartem0307@gmail.com>
2023-11-12 22:13:07 +0100

    add user Subscription & Payment

--- user/models.py
+++ user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
@@ -25,6 +26,7 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -60,7 +62,75 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         db_table = 'user'
 
     def __str__(self):
-        return self.email
+        return f'<{self.__class__.__name__}>: {self.email}'
 
+    def __repr__(self):
+        return self.__str__()
+
+
+class Payment(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="User",
+        related_name="payments"
+    )
+
+    subscription = models.ForeignKey(
+        'Subscription',
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Subscription",
+    )
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Amount Paid",
+    )
+    status = models.CharField(
+        max_length=50,
+        verbose_name="Payment Status",
+        choices=(
+            ('completed', 'Completed'),
+            ('pending', 'Pending'),
+            ('failed', 'Failed'),
+            ('refunded', 'Refunded'),
+        ),
+        default='completed',
+    )
+    date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Payment Date",
+    )
+
+    def __str__(self):
+        return f'<{self.__class__.__name__}>: {self.user}-{self.subscription}-{self.date}'
+
+    def __repr__(self):
+        return self.__str__()
+
+    class Meta:
+        db_table = 'user_payment'
+        verbose_name = "Payment"
+        verbose_name_plural = "Payments"
+        ordering = ['-date']
+
+
+class Subscription(models.Model):
+
+    title = models.CharField(max_length=10, verbose_name="Subscription Level")
+    description = models.TextField(verbose_name="Description")
+    ads_period_min = models.PositiveSmallIntegerField(verbose_name="Min ads period")
+    ads_period_max = models.PositiveSmallIntegerField(verbose_name="Max ads period")
+    managers_per_day = models.PositiveIntegerField(verbose_name="Managers per day")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    is_active = models.BooleanField(default=0)
+
+    class Meta:
+        db_table = 'subscription'
+        verbose_name = "Subscription"
+        verbose_name_plural = "Subscriptions"
+        ordering = ['-price']
 
 
